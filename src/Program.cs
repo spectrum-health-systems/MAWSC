25389013cdaf163716292f3666b7b95c755ef872
Developer <development@aprettycoolprogram.com>
2022-01-25 15:02:48 -0500

    Refresh

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// MAWSC - MAWS Commander: Command-line utilities for MAWS
+// https://github.com/aprettycoolprogram/MAWSC
+// Copyright (C) 2015-2022 A Pretty Cool Program
+// Licensed under Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+
+// Entry point for MAWSC
+// b220125.132925
+
+MACL.Logging.WriteLog();
+
+//Console.WriteLine("Hello, World!");
