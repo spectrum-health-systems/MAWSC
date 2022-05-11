ad9d3ca83a2e9fa08da48486bc75d9e2014a561a
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2022-05-11 11:35:46 -0400

    v1.2.0.0-b220511+dev113517

--- src/MAWSC.cs
+++ src/MAWSC.cs
@@ -4,7 +4,7 @@
 // LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
 //          Copyright 2021 A Pretty Cool Program
 
-// v1.2.0.0-b220511+dev104717
+// v1.2.0.0-b220511+dev113517
 
 /* ============================================================================================
  * About this project
