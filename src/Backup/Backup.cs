0839416ee51221946f3ebc25f7dddc82fe1e6082
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2022-05-25 12:03:40 -0400

    Update

---
+++ src/Backup/Backup.cs
@@ -0,0 +1,18 @@
+﻿namespace MAWSC
+{
+    internal class Backup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mawscSettings"></param>
+        internal static void VerifySessionBackupDirectory(MAWSC.Configuration.Settings mawscSettings)
+        {
+            var sessionBackupDirectory = $"{mawscSettings.BackupDirectory}{mawscSettings.SessionTimestamp}";
+
+            Du.WithDirectory.ConfirmDirectoryExists(sessionBackupDirectory);
+
+            MAWSC.Log.Export.ToConsole(MAWSC.Log.Message.SessionBackupDirectoryVerify());
+        }
+    }
+}
